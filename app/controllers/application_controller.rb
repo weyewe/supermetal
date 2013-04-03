8a81e67c10667031f9e50162d1e99cb643bd58b4
weyewe <w.yunnal@gmail.com>
2013-04-03 19:58:19 +0700

    skip aupthenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include TheRole::Requires
+  # include TheRole::Requires
   protect_from_forgery
   
   
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
     render :text => 'access_denied: requires an role' and return
   end
 
-  alias_method :login_required,     :authenticate_user!
-  alias_method :role_access_denied, :access_denied
+  # alias_method :login_required,     :authenticate_user!
+  # alias_method :role_access_denied, :access_denied
     
     
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   
   layout :layout_by_resource
   def layout_by_resource
@@ -24,26 +24,7 @@ class ApplicationController < ActionController::Base
   end
   
   def after_sign_in_path_for(resource)
-    # active_job_attachment  = current_user.active_job_attachment 
-    # if current_user.has_role?( :branch_manager, active_job_attachment)
-    #   puts "user has role branch_manager!\n"*10
-    #   return new_group_loan_product_url
-    # end
-    # 
-    # if current_user.has_role?(:loan_officer, active_job_attachment)
-    #   puts "user has role loan_officer!\n"*10
-    #   return new_member_url
-    # end
-    # 
-    # if current_user.has_role?(:field_worker, active_job_attachment)
-    #   puts "user has role field_worker!\n"*10
-    #   return select_group_loan_for_weekly_meeting_attendance_marking_url
-    # end
-    # 
-    # if current_user.has_role?(:cashier, active_job_attachment)
-    #   puts "user has role field_worker!\n"*10
-    #   return select_group_loan_for_loan_disbursement_url
-    # end
+
     
     return root_url 
     
